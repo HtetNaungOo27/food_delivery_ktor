6bf646aa68f1f1e783bad3437ec38ee52c6548a4
Sandaku152025 <sandaku2005@gmail.com>
2026-08-11 23:09:52 +0630

    Update project

--- src/main/kotlin/com/codewithfk/configs/StripeConfig.kt
+++ src/main/kotlin/com/codewithfk/configs/StripeConfig.kt
@@ -1,7 +1,7 @@
 package com.codewithfk.configs
 
 object StripeConfig {
-    const val publishableKey = ""
-    const val secretKey = ""
-    const val webhookSecret = ""
+    const val publishableKey = "pk_test_51U3CDVAqWjqibd203JDyLqjT6beMGhv0oVXRoCchqcVBdKiZFWXlMpbx6dUq9wfFFNCM2pemirN7NbjPkyz49qNF00deFC5pP1"
+    const val secretKey = "sk_test_51U3CDVAqWjqibd205Wwi6NTsxSqMBNKO1xKmdNSqFcVcLrQagmrtiKF8YURUoKG0RydCdCPObkvfNxAY4slg9kY700LCbf8bON"
+    const val webhookSecret = "whsec_f7de7b10a5b3e14926ec9c157b39577a0200268bbef881d74387d3e320398b32"
 } 
